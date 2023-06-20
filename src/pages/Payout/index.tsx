a01f9eea450a2a1d63db25fd2d4fd4166500bcba
Yago Henrique <yagohenriquest@gmail.com>
2023-06-20 15:10:35 -0300

    fixed payout

--- src/pages/Payout/index.tsx
+++ src/pages/Payout/index.tsx
@@ -1,9 +1,13 @@
 import {
+  Bag,
   Bank,
   CreditCard,
   CurrencyDollar,
   MapPinLine,
+  Minus,
   Money,
+  Plus,
+  Trash,
 } from 'phosphor-react'
 import {
   PayoutContainer,
@@ -19,12 +23,41 @@ import {
   FieldInputsContainer,
   PayMethodContainer,
   ButtonsContainer,
+  ChartContainer,
+  NoItemsOnCart,
+  ProductContainer,
+  QuantityContainer,
+  ButtonAddContainer,
+  RemoveContainer,
+  ValueCoffeContainer,
+  PriceValueContainer,
+  ConfirmOrderContainer,
 } from './styles'
-import { ChartPaymentContainer } from './Chart'
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { ChartContext } from '../../contexts/ChartAndCoffes'
 
 export function Payout() {
+  const { products, setAddQuantity, removeQuantity, removeFromChart } =
+    useContext(ChartContext)
+  const itensOnChart = products.filter((product) => product.isOnChart === true)
+
+  const totalPrice = itensOnChart.reduce((acc, currentItem) => {
+    const subtotal = currentItem.price * currentItem.quantity
+    return acc + subtotal
+  }, 0)
+
+  const entrega = 3.5
+  const totalWithSend = totalPrice + entrega
+
+  const navigate = useNavigate()
+
+  function handleRedirect() {
+    navigate('/Completedbuy')
+  }
+
   return (
-    <PayoutContainer>
+    <PayoutContainer onSubmit={() => handleRedirect()}>
       <main>
         <h1>Complete seu pedido</h1>
         <FormContainer>
@@ -81,7 +114,76 @@ export function Payout() {
       </main>
       <aside>
         <h1>Cafés selecionados</h1>
-        <ChartPaymentContainer />
+        <ChartContainer>
+          {itensOnChart.length === 0 ? (
+            <NoItemsOnCart>
+              <Bag size={32} color="#D7D5D5" />
+              <h1>Você não possui itens no carrinho</h1>
+            </NoItemsOnCart>
+          ) : (
+            itensOnChart.map((cofe: any) => {
+              return (
+                <ProductContainer key={cofe.id}>
+                  <img src={cofe.src} alt={cofe.name} />
+                  <div>
+                    <p>{cofe.name}</p>
+                    <QuantityContainer>
+                      <ButtonAddContainer>
+                        <button
+                          type="button"
+                          onClick={() => removeQuantity(cofe.id)}
+                        >
+                          <Minus size={17} color="#8047F8" />
+                        </button>
+
+                        <p>{cofe.quantity}</p>
+
+                        <button
+                          type="button"
+                          onClick={() => setAddQuantity(cofe.id)}
+                        >
+                          <Plus size={17} color="#8047F8" />
+                        </button>
+                      </ButtonAddContainer>
+                      <RemoveContainer
+                        type="button"
+                        onClick={() => removeFromChart(cofe.id)}
+                      >
+                        <Trash size={17} color="#8047F8" />
+                        REMOVER
+                      </RemoveContainer>
+                      <ValueCoffeContainer>
+                        <p>
+                          R${' '}
+                          {cofe.price.toFixed(2).toString().replace('.', ',')}
+                        </p>
+                      </ValueCoffeContainer>
+                    </QuantityContainer>
+                  </div>
+                </ProductContainer>
+              )
+            })
+          )}
+          <PriceValueContainer>
+            <div>
+              <p>Total de Itens</p>
+              <p>R$ {totalPrice.toFixed(2).toString().replace('.', ',')}</p>
+            </div>
+            <div>
+              <p>Entrega</p>
+              <p>R$ {entrega.toFixed(2).toString().replace('.', ',')}</p>
+            </div>
+            <div>
+              <h1>Total</h1>
+              <h1>
+                R$ {totalWithSend.toFixed(2).toString().replace('.', ',')}
+              </h1>
+            </div>
+            <ConfirmOrderContainer type="submit">
+              CONFIRMAR PEDIDO
+            </ConfirmOrderContainer>
+          </PriceValueContainer>
+        </ChartContainer>
       </aside>
     </PayoutContainer>
   )
--- src/pages/Payout/styles.ts
+++ src/pages/Payout/styles.ts
@@ -18,7 +18,7 @@ export const ChartContainer = styled.div`
   border-radius: 6px 44px;
 `
 
-export const FormContainer = styled.form`
+export const FormContainer = styled.div`
   background: ${(props) => props.theme['base-card']};
   padding: 2.5rem;
   width: 640px;
@@ -133,5 +133,115 @@ export const ButtonsContainer = styled.div`
       background: ${(props) => props.theme['base-hover']};
       transition: 0.4s;
     }
+  }
+`
+
+export const ProductContainer = styled.div`
+  display: flex;
+  gap: 12px;
+  align-items: center;
+  margin-bottom: 30px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid ${(props) => props.theme['base-button']};
+  img {
+    max-width: 64px;
+  }
+`
+
+export const QuantityContainer = styled.div`
+  display: flex;
+  gap: 6px;
+`
+
+export const ButtonAddContainer = styled.div`
+  display: flex;
+  gap: 7px;
+  background: ${(props) => props.theme['base-button']};
+  width: fit-content;
+  padding: 6px;
+  border-radius: 6px;
+
+  p {
+    font-size: 1rem;
+  }
+
+  button {
+    background: none;
+    border: 0;
+    cursor: pointer;
+  }
+`
+
+export const RemoveContainer = styled.button`
+  display: flex;
+  align-items: center;
+  padding: 6px;
+  border: 0;
+  background: ${(props) => props.theme['base-button']};
+  border-radius: 6px;
+  color: ${(props) => props.theme['base-text']};
+  cursor: pointer;
+  &:hover {
+    background-color: ${(props) => props.theme['purple-light']};
+    transition: 0.5s;
+  }
+`
+
+export const ValueCoffeContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  font-family: 'Baloo 2';
+  font-size: 20px;
+`
+
+export const PriceValueContainer = styled.div`
+  div {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 12px;
+  }
+  div:first-child {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 24px;
+  }
+
+  div:nth-child(3) {
+    margin-bottom: 33px;
+  }
+`
+
+export const ConfirmOrderContainer = styled.button`
+  width: 100%;
+  padding: 12px;
+  font-weight: 700;
+  background: ${(props) => props.theme.yellow};
+  border: 0;
+  border-radius: 6px;
+  color: ${(props) => props.theme.white};
+  cursor: pointer;
+  &:not(:disabled):hover {
+    background-color: ${(props) => props.theme['yellow-dark']};
+    transition: 0.5s;
+  }
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
+`
+
+export const NoItemsOnCart = styled.div`
+  border: 1px solid ${(props) => props.theme['base-hover']};
+  height: 100px;
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+  h1 {
+    margin: 0px;
+    color: ${(props) => props.theme['base-hover']};
   }
 `
